20f19ff1a35efb2c532b268f58c68bb455d812fa
Oleksiy Nikolayenko <mennekis@gmail.com>
2023-07-07 18:01:06 +0300

    lesson1

---
+++ lesson_1/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Button from './components/button';
+import Input from './components/input';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+   <h1>Hello REACT!!! Next station Typescript ;)</h1>
+    </div>
+    <Input text="Чего тебе надобно пиши тута"/>
+<div><Button text="Hello"/>
+    <Button text="Click"/>
+    <Button text="Delete"/>
+    <Button text="Submit"/></div>
+    
+
+    </>
+  );
+}
+
+export default App;
---
+++ lesson_1/src/components/input.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Input = (props) => {
+  return (
+    <input placeholder={props.text}></input>
+  )
+}
+
+export default Input
